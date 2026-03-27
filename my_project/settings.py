c174ed0d0473e5d50bf03755eca73ce60d94994d
crucioltheresa <mtcruciol@icloud.com>
2026-03-27 15:04:48 +0000

    Prepare project for Heroku deployment
    - Added Procfile to run the app with gunicorn
    - Set DEBUG to False and configured ALLOWED_HOSTS for Heroku and local dev
    - Updated requirements.txt to include gunicorn and setuptools, removing unused dev dependencies
    - Reformatted long lines in settings.py for readability

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f5j4n%t@wncd&6q0m%ey5$eoid0ji2@lv8q$$^o=qn^bv*6zd^'
+SECRET_KEY = (
+    'django-insecure-f5j4n%t@wncd&6q0m%ey5$eoid0ji2@lv8q$$^o=qn^bv*6zd^'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -87,16 +92,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
